62990c8056e257512b235e2c9b18385759dd3d7e
Samuel Dawant <samrenfou@hotmail.com>
2020-05-02 16:36:19 +0200

    Fix crash

--- scripts/prefabs/blacktim.lua
+++ scripts/prefabs/blacktim.lua
@@ -8,11 +8,11 @@ local prefabs = {}
 
 -- Custom starting inventory
 local start_inv =
-    {
+{
     default =
-        {
-        },
-    }
+    {
+    },
+}
 
 local function doHappinessDamage(inst)
     local HAPPYRATE = .1
@@ -30,8 +30,8 @@ local function becomeveryhappy(inst)
     inst.components.talker:Say(GetString(inst, "ANNOUNCE_BECOMEVERYHAPPY"))
 
     if inst.happinessdot ~= nil then
-        inst.happinessdot:Cancel()
-        inst.happinessdot = nil
+	inst.happinessdot:Cancel()
+	inst.happinessdot = nil
     end
 
     inst.happinessdot = inst:DoPeriodicTask(1/3, doHappinessDamage)
@@ -43,18 +43,18 @@ local function becomehappy(inst)
     if inst.happiness == "happy" then
         return
     end
-    }
-inst.components.skinner:SetSkinMode("happy_skin", "blacktim_happy")
-inst.components.talker:Say(GetString(inst, "ANNOUNCE_BECOMEHAPPY"))
 
-if inst.happinessdot ~= nil then
-    inst.happinessdot:Cancel()
-    inst.happinessdot = nil
-end
+    inst.components.skinner:SetSkinMode("happy_skin", "blacktim_happy")
+    inst.components.talker:Say(GetString(inst, "ANNOUNCE_BECOMEHAPPY"))
 
-inst.happinessdot = inst:DoPeriodicTask(1,doHappinessDamage)
+    if inst.happinessdot ~= nil then
+	inst.happinessdot:Cancel()
+	inst.happinessdot = nil
+    end
 
-inst.happiness = "happy"
+    inst.happinessdot = inst:DoPeriodicTask(1,doHappinessDamage)
+
+    inst.happiness = "happy"
 end
 
 local function becomenormal(inst)
@@ -66,8 +66,8 @@ local function becomenormal(inst)
     inst.components.talker:Say(GetString(inst, "ANNOUNCE_BACKTONORMAL"))
 
     if inst.happinessdot ~= nil then
-        inst.happinessdot:Cancel()
-        inst.happinessdot = nil
+	inst.happinessdot:Cancel()
+	inst.happinessdot = nil
     end
 
     inst.happiness = "normal"
@@ -100,24 +100,18 @@ end
 
 -- When the character is revived from human
 local function onbecamehuman(inst)
-    -- Set speed when not a ghost (optional)
-    inst.components.locomotor:SetExternalSpeedMultiplier(inst, "blacktim_speed_mod", 1)
+	-- Set speed when not a ghost (optional)
+	inst.components.locomotor:SetExternalSpeedMultiplier(inst, "blacktim_speed_mod", 1)
 
-    inst:ListenForEvent("sanitydelta", onsanitychange)
+        inst:ListenForEvent("sanitydelta", onsanitychange)
 
 end
 
 local function onbecameghost(inst)
-    -- Remove speed modifier when becoming a ghost
+	-- Remove speed modifier when becoming a ghost
+    inst.happiness="normal"
     inst.components.locomotor:RemoveExternalSpeedMultiplier(inst, "blacktim_speed_mod")
-
     inst.components.skinner:SetSkinMode("ghost_skin", "ghost_blacktim_build")
-    if inst.happinessdot ~= nil then
-        inst.happinessdot:Cancel()
-        inst.happinessdot = nil
-    end
-
-    inst.happiness = "normal"
 end
 
 -- When loading or spawning the character
@@ -148,36 +142,36 @@ end
 
 -- This initializes for both the server and client. Tags can be added here.
 local common_postinit = function(inst)
-    -- Minimap icon
-    inst.MiniMapEntity:SetIcon( "blacktim.tex" )
+	-- Minimap icon
+	inst.MiniMapEntity:SetIcon( "blacktim.tex" )
 end
 
 -- This initializes for the server only. Components are added here.
 local master_postinit = function(inst)
-    -- choose which sounds this character will play
-    inst.soundsname = "maxwell"
+	-- choose which sounds this character will play
+	inst.soundsname = "maxwell"
 
-    inst.happiness = "normal"
-    inst.happinessdot = nil
+        inst.happiness = "normal"
+        inst.happinessdot = nil
 
-    inst.components.sanity.night_drain_mult = 0
-    inst.components.sanity.neg_aura_mult = 0.7
+        inst.components.sanity.night_drain_mult = 0
+        inst.components.sanity.neg_aura_mult = 0.7
 
-    -- Uncomment if "wathgrithr"(Wigfrid) or "webber" voice is used
+	-- Uncomment if "wathgrithr"(Wigfrid) or "webber" voice is used
     --inst.talker_path_override = "dontstarve_DLC001/characters/"
 
-    -- Stats
-    inst.components.health:SetMaxHealth(75)
-    inst.components.hunger:SetMax(150)
-    inst.components.sanity:SetMax(150)
+	-- Stats
+	inst.components.health:SetMaxHealth(75)
+	inst.components.hunger:SetMax(150)
+	inst.components.sanity:SetMax(150)
 
-    -- Damage multiplier (optional)
-    inst.components.combat.damagemultiplier = 0.8
+	-- Damage multiplier (optional)
+        inst.components.combat.damagemultiplier = 0.8
 
-    -- Hunger rate (optional)
-    inst.components.hunger.hungerrate = 1 * TUNING.WILSON_HUNGER_RATE
+	-- Hunger rate (optional)
+	inst.components.hunger.hungerrate = 1 * TUNING.WILSON_HUNGER_RATE
 
-    inst.OnLoad = onload
+	inst.OnLoad = onload
     inst.OnNewSpawn = onspawn
 
 end
